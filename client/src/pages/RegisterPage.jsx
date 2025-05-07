93b5c1613dc76a09781a4c7c3ac6850f180a3517
dci-student <aleksandrabialas94@gmail.com>
2025-05-07 19:37:48 +0200

    RegisterPage update

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "../styles/Register.scss";
+import { useState } from "react";
 
 const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    profilImage: null,
+  });
+  console.log(formData);
+
+  const handleChange = (e) => {
+    const { name, value, files } = <e className="target"></e>;
+  };
+
   return (
     <div className="register">
       <div className="register_content">
@@ -9,21 +24,36 @@ const RegisterPage = () => {
           <input
             type="text"
             placeholder="First Name"
-            name="firstname"
+            name="firstName"
+            value={formData.firstName}
             required
           />
-          <input type="text" placeholder="Last Name" name="lastname" required />
-          <input type="email" placeholder="E-mail" name="email" required />
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            value={formData.lastName}
+            required
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            name="email"
+            value={formData.email}
+            required
+          />
           <input
             type="password"
             placeholder="Password"
             name="password"
+            value={formData.password}
             required
           />
           <input
             type="password"
             placeholder="Confirm Password"
-            name="confirmpassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
             required
           />
           <input
